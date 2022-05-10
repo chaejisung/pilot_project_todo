2819674256ccedc861a18addf72ed657ba8af7e4
채지성 <jschae02@khu.ac.kr>
2022-05-10 23:30:39 +0900

    third

--- server.js
+++ server.js
@@ -2,30 +2,39 @@
 
 const express = require('express');
 const app = express();
-const ejs = require('ejs');
-
-app.set('view engine', 'ejs');
-app.set('views', './static');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.json());
 
 app.listen(8080, function () {
     console.log('listening on 8080')
 })
 
-app.get('/', function(req, res){
-    res.render('index.ejs', {items:items});
-})
-
 let items=[];
 
+app.get('/', function(req, res){
+    res.send(items);
+});
 app.post('/post', function(req, res){
-    let item = req.body.text;
-    console.log(item);
-    if(item != ""){items.push(item);}
-    console.log("items", items);
-
-    res.render('index.ejs', {items: items});
+    let item = req.body;
+    console.log(item.id);
+    console.log(item.text);
     
+    if(item != ""){ items.push(item); }
+    console.log("items: ", items);
+
+    res.send(items);
+    
+});
+app.delete('/item/:id', function(req, res){
+    let id = req.params.id;
+    console.log(id);
+
+    items.forEach((elem, index)=>{
+        if(elem.id == id){ items.splice(index, 1); }
+    });
+    console.log(items);
+
+    res.send(items);
 });
